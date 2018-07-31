db958b7516f8e9e5bb9855c13b42201cb95c6e59
CoderDKP <1578098176@qq.com>
2018-07-31 23:53:26 +0800

    ded

---
+++ LargeVaryingGear/moreProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  moreProject
+//
+//  Created by 刘湘 on 2018/1/23.
+//  Copyright © 2018年 刘湘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LargeVaryingGear/moreProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  moreProject
+//
+//  Created by 刘湘 on 2018/1/23.
+//  Copyright © 2018年 刘湘. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
